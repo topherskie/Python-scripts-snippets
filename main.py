909e25bd83598822b05ac3cae09fb931474ee59c
topherskie <73430419+topherskie@users.noreply.github.com>
2021-03-21 18:46:32 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# Basic calculator from python
+import functions
+num1 = input('Enter 1st Digit: ')
+op = input('Enter math operator: ')
+num2 = input('Enter 2nd Digit: ')
+result = 0.0
+
+if op == '+':
+    result = float(num1) + float(num2)
+    print('The result of Addition: ', result)
+elif op == '-':
+    result = float(num1) - float(num2)
+    print('The result of Subtraction: ', result)
+elif op == '*':
+    result = float(num1) * float(num2)
+    print('The result of Multiplication: ', result)
+elif op == '/':
+    result = float(num1) / float(num2)
+    print('The result of Division: ', result)
+elif op == '%':
+    result = float(num1) % float(num2)
+    print('The result of Modulo: ', result)
+else:
+    print('Invalid inputs!')
+    print(functions.name)
